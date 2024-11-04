4d319edea9e2e3992cdc61ae69c35ccb5550fee9
Kirill Platonov <platonovk1998@ya.ru>
2024-11-05 01:11:52 +0500

    low poly far desert with janky seam

--- scenes/DesertTerrain/DesertTerrain.gd
+++ scenes/DesertTerrain/DesertTerrain.gd
@@ -4,7 +4,8 @@ class_name DesertTerrain
 
 @export var wind_current_scene: PackedScene
 @export var rock_structure_scene: PackedScene
-@export var sand_material: StandardMaterial3D
+@export var sand_near_material: StandardMaterial3D
+@export var sand_far_material: StandardMaterial3D
 
 @onready var mesh_update_area: Area3D = $MeshUpdateArea
 @onready var mesh_update_shape: CollisionShape3D = $MeshUpdateArea/MeshUpdateShape
@@ -16,12 +17,14 @@ class_name DesertTerrain
 
 var ACTIVE_COLLISION_RADIUS := 6
 
-var current_terrain_mesh: MeshInstance3D
+var current_terrain_near_mesh: MeshInstance3D
+var current_terrain_far_mesh: MeshInstance3D
 var current_terrain_shape: CollisionShape3D
 var current_terrain_structures: Node3D
 var thread_pool_task_ids: Array[int] = []
 var is_swapping = {
-	"mesh": false,
+	"near_mesh": false,
+	"far_mesh": false,
 	"shape": false,
 	"structures": false,
 }
@@ -32,7 +35,7 @@ func _ready():
 	shape_update_area.body_exited.connect(_on_shape_update_area_body_exited)
 	structure_update_area.body_exited.connect(_on_structure_update_area_body_exited)
 	
-	mesh_update_shape.shape.radius = Global.GENERATOR_RADIUS / 2
+	mesh_update_shape.shape.radius = Global.HIGH_POLY_MESH_RADIUS / 2
 	shape_update_shape.shape.radius = ACTIVE_COLLISION_RADIUS / 2
 	structure_update_shape.shape.radius = Global.ACTIVE_STRUCTURE_RADIUS / 2
 	
@@ -41,8 +44,13 @@ func _ready():
 	thread_pool_task_ids.append(
 		WorkerThreadPool.add_task(_swap_terrain_structures.bind(Vector3.ZERO)))
 	
-	sand_material.distance_fade_min_distance = Global.GENERATOR_RADIUS / 4
-	sand_material.distance_fade_max_distance = sand_material.distance_fade_min_distance - 50
+	sand_near_material.distance_fade_min_distance = Global.HIGH_POLY_MESH_RADIUS / 2
+	sand_near_material.distance_fade_max_distance = (
+		sand_near_material.distance_fade_min_distance - 50)
+	
+	sand_far_material.distance_fade_max_distance = Global.HIGH_POLY_MESH_RADIUS / 2 - 50
+	sand_far_material.distance_fade_min_distance = (
+		sand_near_material.distance_fade_min_distance - 100)
 
 
 func _on_mesh_update_area_body_exited(body):
@@ -50,11 +58,16 @@ func _on_mesh_update_area_body_exited(body):
 	
 	mesh_update_area.global_position = body.global_position
 	
-	if is_swapping.mesh: return
-	is_swapping.mesh = true
-	
-	thread_pool_task_ids.append(
-		WorkerThreadPool.add_task(_swap_terrain_mesh.bind(body.global_position)))
+	if not is_swapping.near_mesh:
+		is_swapping.mesh = true
+		
+		thread_pool_task_ids.append(
+			WorkerThreadPool.add_task(_swap_terrain_mesh.bind(body.global_position, true)))
+	if not is_swapping.far_mesh:
+		is_swapping.far_mesh = true
+		
+		thread_pool_task_ids.append(
+			WorkerThreadPool.add_task(_swap_terrain_mesh.bind(body.global_position, false)))
 
 
 func _on_shape_update_area_body_exited(body):
@@ -80,20 +93,28 @@ func _on_structure_update_area_body_exited(body):
 		WorkerThreadPool.add_task(_swap_terrain_structures.bind(body.global_position)))
 
 
-func _swap_terrain_mesh(origin: Vector3):
-	origin = snapped(origin, Vector3(Global.MESH_STEP, Global.MESH_STEP, Global.MESH_STEP))
-	
+func _swap_terrain_mesh(origin: Vector3, is_near_mesh: bool):
 	var new_terrain_mesh := MeshInstance3D.new()
 	
 	var vertex_array := PackedVector3Array()
 	var normals_array := PackedVector3Array()
-	_add_terrain_to_arrays(
-		vertex_array,
-		-Global.GENERATOR_RADIUS,
-		Global.GENERATOR_RADIUS,
-		origin,
-		normals_array
-	)
+	if is_near_mesh:
+		_add_terrain_to_arrays(
+			vertex_array,
+			-Global.HIGH_POLY_MESH_RADIUS,
+			Global.HIGH_POLY_MESH_RADIUS,
+			origin,
+			normals_array
+		)
+	else:
+		_add_terrain_to_arrays(
+			vertex_array,
+			-Global.LOW_POLY_MESH_RADIUS,
+			Global.LOW_POLY_MESH_RADIUS,
+			origin,
+			normals_array,
+			Global.FAR_MESH_STEP,
+		)
 	
 	var arrays = []
 	arrays.resize(Mesh.ARRAY_MAX)
@@ -103,17 +124,21 @@ func _swap_terrain_mesh(origin: Vector3):
 	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
 	new_terrain_mesh.mesh = array_mesh
 	
-	new_terrain_mesh.material_override = sand_material
+	if is_near_mesh:
+		new_terrain_mesh.material_override = sand_near_material
+		if current_terrain_near_mesh: current_terrain_near_mesh.queue_free()
+		current_terrain_near_mesh = new_terrain_mesh
+		is_swapping.near_mesh = false
+	else:
+		new_terrain_mesh.material_override = sand_far_material
+		if current_terrain_far_mesh: current_terrain_far_mesh.queue_free()
+		current_terrain_far_mesh = new_terrain_mesh
+		is_swapping.far_mesh = false
 	
 	terrain_body.add_child.call_deferred(new_terrain_mesh)
-	if current_terrain_mesh: current_terrain_mesh.queue_free()
-	current_terrain_mesh = new_terrain_mesh
-	
-	is_swapping.mesh = false
 
 
 func _swap_terrain_shape(origin: Vector3):
-	origin = snapped(origin, Vector3(Global.MESH_STEP, Global.MESH_STEP, Global.MESH_STEP))
 	var new_terrain_shape := CollisionShape3D.new()
 	
 	var vertex_array = PackedVector3Array()
@@ -186,25 +211,28 @@ func _add_terrain_to_arrays(
 	range_min: float,
 	range_max: float,
 	origin: Vector3,
-	normals_array = null
+	normals_array = null,
+	step := Global.MESH_STEP,
 ):
-	for x_offset in range(range_min, range_max, Global.MESH_STEP):
-		for z_offset in range(range_min, range_max, Global.MESH_STEP):
+	origin = snapped(origin, Vector3(step, step, step))
+	
+	for x_offset in range(range_min, range_max, step):
+		for z_offset in range(range_min, range_max, step):
 			var bot_left = origin
 			bot_left.x += x_offset
 			bot_left.z += z_offset
 			bot_left = Global.get_terrain_point_from_x_z(bot_left.x, bot_left.z)
 			
 			var bot_right = bot_left
-			bot_right.x += Global.MESH_STEP
+			bot_right.x += step
 			bot_right = Global.get_terrain_point_from_x_z(bot_right.x, bot_right.z)
 			
 			var top_left = bot_left
-			top_left.z -= Global.MESH_STEP
+			top_left.z -= step
 			top_left = Global.get_terrain_point_from_x_z(top_left.x, top_left.z)
 			
 			var top_right = bot_right
-			top_right.z -= Global.MESH_STEP
+			top_right.z -= step
 			top_right = Global.get_terrain_point_from_x_z(top_right.x, top_right.z)
 			
 			Global.add_quad_to_vertex_array(
